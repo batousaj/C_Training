f06daa62eff190fe728289ef059e639c6db223d0
Vu Hoang Thien (RBVH/EPS23) <ovt81hc@bosch.com>
2020-12-19 21:34:29 +0700

    first init file for training

---
+++ header/ht_countingsort_head.h
@@ -0,0 +1,18 @@
+/**
+ * @file ht_countingsort_head.h
+ * @author Thien Vu (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-12-19
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+
+#ifndef ht_countingsort_head_H
+#define ht_countingsort_head_H
+
+#include <stdint.h>
+
+#endif // * define ht_countingsort_head_H //
---
+++ src/ht_main_debug.c
@@ -0,0 +1,23 @@
+/**
+ * @file ht_main_debug.c
+ * @author Thien Vu (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-12-19
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+
+#include <stdio.h>
+#include "ht_countingsort_head.h"
+
+
+#define ht_constantvalue_flash 0x01
+
+int main() {
+
+    printf(unit8(ht_constantvalue_flash));
+    return 0;
+}
