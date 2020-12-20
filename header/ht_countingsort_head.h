6864616381c08823b69f2b2b85c36bd349d18045
Vu Hoang Thien (RBVH/EPS23) <ovt81hc@bosch.com>
2020-12-20 14:24:11 +0700

    Using training for Define Enum, Marco, Structure, Extern

--- header/ht_countingsort_head.h
+++ header/ht_countingsort_head.h
@@ -18,6 +18,33 @@ extern "C" {
 
 #include <stdint.h>
 
+#define ht_constantvalue_flash 0x02
+
+
+extern int stack_counter;
+
+extern void ht_countingsort(void);
+
+typedef struct {
+    uint32_t ht_list_u32;
+    uint8_t *ht_number_u8;
+
+} ht_maxswing_tmp;
+
+enum {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday,
+    No_day
+    
+} ht_list_day_tmp;
+
+
+
 #ifdef __cplusplus
 }
 #endif // * extern the libary between C and C++
--- src/ht_main_debug.c
+++ src/ht_main_debug.c
@@ -14,13 +14,27 @@
 #include "ht_countingsort_head.h"
 
 
-#define ht_constantvalue_flash 2
-uint8_t a;
+uint8_t a,tmp ;
+uint8_t count = 8;
 
-int main() {
+/**
+ * @brief 
+ * 
+ */
+#ifdef ht_constantvalue_flash 
+    uint8_t *c = &count;
+#endif
+
+int main(void) {
+
+    ht_countingsort();
 
     printf ("hello world\n");
-    a = ht_constantvalue_flash + 2 ;
-    printf(a);
+    printf ("%d \n",c);
+
+    tmp = (uint8_t)((ht_constantvalue_flash << 1u ) | 0x01 ) + 2;
+    a = *c + 2 + tmp;
+
+    printf ("%d",a);
     return 0;
 }
