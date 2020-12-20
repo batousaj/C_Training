1b85cb7d6ea4169bee13908dc0fb4fb6d639062e
Vu Hoang Thien (RBVH/EPS23) <ovt81hc@bosch.com>
2020-12-20 16:00:56 +0700

    Child Header file in Project

---
+++ header/ht_main_head.h
@@ -0,0 +1,26 @@
+/**
+ * @file ht_countingsort_head.h
+ * @author Thien Vu (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-12-19
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+
+#ifndef ht_countingsort_head_H
+#define ht_countingsort_head_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+extern void ht_countingsort(void);
+
+#ifdef __cplusplus
+}
+#endif // * extern the libary between C and C++
+#endif // * define ht_countingsort_head_H //
